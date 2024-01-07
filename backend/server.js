ea4fa9fd56b4a2e8c9849253a95fba4673fa14e2
Shubhi Agrawal <shubhi.agrawal_cs19@gla.ac.in>
2024-01-07 16:31:55 +0530

    created server at port 8080 and created endpoints for GET and PUT api

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./db');
+const Configuration = require('./config');
+
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json()); // Parse JSON request body
+
+connectDB();
+
+// Task 1: GET endpoint to take configID and return 2d array
+app.get('/api/configurations/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const configuration = await Configuration.findById(id);
+
+    if (configuration) {
+      res.json(configuration.data);
+    } else {
+      res.status(404).json({ error: 'Configuration ID not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error' });
+  }
+});
+
+// Task 2: PUT endpoint to update remark
+
+app.put('/api/configurations/:id', async (req, res) => {
+  const { id } = req.params;
+  const { remark } = req.body;
+
+  try {
+    const configuration = await Configuration.findById(id);
+
+    if (configuration) {
+      configuration.remark = remark;
+      await configuration.save();
+      res.json({ message: 'success' });
+    } else {
+      res.status(404).json({ error: 'Configuration ID not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: ' Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
